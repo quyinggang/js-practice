7ca839a0e90732896b1bb6012a3d0ef3583ad8e3
quyinggang <yinggang.qu@gmail.com>
2022-06-25 18:59:20 +0800

    refactor: zoomable

--- zoomable/index.js
+++ zoomable/index.js
@@ -1,16 +1,11 @@
 class Zoomable {
   constructor($el, options) {
-    if (!($el instanceof HTMLElement)) {
-      throw new Error("非法挂载元素");
-    }
-    this.$el = $el;
+
     this.config = options || {};
-    this.zoomElement = null;
-    this.size = { width: 0, height: 0 };
-    this.offset = { x: 0, y: 0 };
-    this.scale = 1;
-    this.minScale = 0.2;
-    this.maxScale = 8;
+    this.zoomElement = $el;
+    this.transform = { x: 0, y: 0, scale: 1 }
+    this.minZoom = 0.2;
+    this.maxZoom = 8;
     this.dragging = false;
     this.zooming = false;
     this.init();
@@ -18,38 +13,31 @@ class Zoomable {
   }
 
   init() {
-    this.render();
     const zoomElement = this.zoomElement;
-    const { width, height, top, left } = zoomElement.getBoundingClientRect();
-    this.size = { width, height };
-    this.offset = { x: top, y: left };
+    const { top, left } = zoomElement.getBoundingClientRect();
+    this.transform = { x: top, y: left, scale: 1 }
     this.layout();
   }
 
-  render() {
-    const $el = this.$el;
-    const zoomElement = document.createElement("div");
-    zoomElement.className = "zoom-container";
-    zoomElement.innerHTML =
-      '<img class="image" src="https://tse1-mm.cn.bing.net/th/id/R-C.5bad3460c502d864fbba90bdabbbb6ce?rik=k6V%2bk0v8pM%2f0HA&riu=http%3a%2f%2fi.loli.net%2f2020%2f02%2f21%2fp5xi2MvnRt3zoDy.jpg&ehk=vrXS1c4Z7s1gdWHNLNlbZTKbuipqYciCF9Hi81LWLdM%3d&risl=&pid=ImgRaw&r=0" />';
-    this.zoomElement = zoomElement;
-    $el.appendChild(zoomElement);
-  }
-
   layout() {
-    const { offset, zoomElement, scale } = this;
-    zoomElement.style.transform = `translate3d(${offset.x}px, ${offset.y}px, 0) scale(${scale})`;
+    const { transform, zoomElement } = this
+    const { x, y, scale } = transform
+    const style = zoomElement.style
+    style.transformOrigin = '0 0 0';
+    // transform: matrix(a, b, c, d, e, f)
+    style.transform = `matrix(${scale}, 0, 0, ${scale}, ${x}, ${y})`;
   }
 
   drag(originalEvent) {
     if (this.dragging || this.zooming) return;
-    const oldOffset = this.offset;
+    const oldTransform = this.transform;
     const { pageX: startX, pageY: startY } = originalEvent;
     const handleMouseMove = (event) => {
       this.dragging = true;
-      this.offset = {
-        x: Number((event.pageX - startX + oldOffset.x).toFixed(3)),
-        y: Number((event.pageY - startY + oldOffset.y).toFixed(3)),
+      this.transform = {
+        ...oldTransform,
+        x: Number((event.pageX - startX + oldTransform.x).toFixed(3)),
+        y: Number((event.pageY - startY + oldTransform.y).toFixed(3)),
       };
       this.layout();
     };
@@ -63,19 +51,16 @@ class Zoomable {
     window.addEventListener("mouseup", handleMouseUp);
   }
 
+  
+
   getRatio(event) {
     const config = this.config;
-    let initialRatio = Number(config.wheelZoomRatio) || 0.1;
-    let delta = 1;
-    if (event.deltaY) {
-      delta = event.deltaY > 0 ? 1 : -1;
-    } else if (event.wheelDelta) {
-      delta = -event.wheelDelta / 120;
-    } else if (event.detail) {
-      delta = event.detail > 0 ? 1 : -1;
-    }
-    let ratio = -delta * initialRatio;
-    return ratio < 0 ? 1 / (1 - ratio) : 1 + ratio;
+    const speed = Number(config.speed) || 1;
+    let delta = event.deltaY;
+    if (event.deltaMode > 0) delta *= 100;
+    const sign = Math.sign(delta);
+    const deltaAdjustedSpeed = Math.min(0.25, Math.abs(speed * delta / 128));
+    return 1 - sign * deltaAdjustedSpeed;
   }
 
   wheelZoom(event) {
@@ -86,30 +71,43 @@ class Zoomable {
     setTimeout(() => {
       this.zooming = false;
     }, 10);
-    const { size, offset: oldOffset, scale: oldScale, minScale, maxScale } = this
     const ratio = this.getRatio(event);
-    const scale = Math.min(Math.max(oldScale * ratio, minScale), maxScale);
-    if (scale === oldScale) return
+    const { transform, minZoom, maxZoom } = this
+    const { scale: oldScale, x: oldTransformX, y: oldTransformY } = transform
+    const newScale = Math.min(Math.max(oldScale * ratio, minZoom), maxZoom);
+    if (oldScale === newScale) return
     const center = {
       x: event.pageX,
       y: event.pageY
     }
-    // 容器size缩放前后偏差
-    const diff = {
-      x: (ratio - 1) * size.width * 0.5,
-      y: (ratio - 1) * size.height * 0.5,
-    };
-    // 缩放点坐标缩放前后偏差值
-    const centerDiff = {
-      x: (ratio - 1) * center.x,
-      y: (ratio - 1) * center.y
+
+    /**
+     * 另一种方法：
+     * - 容器size缩放前后偏差
+     * - 缩放点坐标缩放前后偏差值
+     * - 保证缩放点与缩放元素缩放前后相对位置不变：缩放前元素偏移offset缩放后的值 + 本次缩放物体大小改变的偏差值 - 当前缩放点的坐标缩放偏差
+     * 
+     * 当不设置缩放中心, css默认的缩放中心是center，此时计算就需要考虑容器自身大小缩放偏差
+     * 如果设置缩放中心始终是左上角，则不需要考虑容器自身大小缩放偏差
+     */
+    // const sizeDiff = {
+    //   x: (ratio - 1) * size.width * 0.5,
+    //   y: (ratio - 1) * size.height * 0.5,
+    // };
+    // const centerDiff = {
+    //   x: (ratio - 1) * center.x,
+    //   y: (ratio - 1) * center.y
+    // }
+    // this.transform = {
+    //   x: oldOffset.x * ratio + diff.x - centerDiff.x,
+    //   y: oldOffset.y * ratio + diff.y - centerDiff.y,
+    //   scale: newScale
+    // };
+    this.transform = {
+      x: center.x - ratio * (center.x - oldTransformX),
+      y: center.y - ratio * (center.y - oldTransformY),
+      scale: newScale
     }
-    // 保证缩放点与缩放元素缩放前后相对位置不变：缩放前元素偏移offset缩放后的值 + 本次缩放物体大小改变的偏差值 - 当前缩放点的坐标缩放偏差
-    this.offset = {
-      x: oldOffset.x * ratio + diff.x - centerDiff.x,
-      y: oldOffset.y * ratio + diff.y - centerDiff.y
-    };
-    this.scale = scale;
     this.layout();
   }
 
